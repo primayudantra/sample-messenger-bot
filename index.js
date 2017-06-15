7120df402245e0230d919fb08f9a63f9637975cc
primayudantra <prima.yudantra@gmail.com>
2017-06-15 14:33:11 +0700

    enabling function

--- index.js
+++ index.js
@@ -15,12 +15,46 @@ app.get('/', function(req,res){
 	res.send("Hi chat bot here")
 })
 
+var token = EAABZBqEOSvHwBAKJzFX3YD7itIQlCPrDZAZBnepIuGapJCzemGdyDJkexZBHCVRw0tOXn6uZCGb9t2BpIHXBmNmu59bTCt0HcTdVo2CWH8UmX8DR3wNdlHZABW8cpbyQhoYQlVBAnIHyVtd8gNDxiwM1ktTf3ZBtSCeGVmE82gKHAZDZD
+
 app.get('/webhook/',function(req,res){
 	if(req.query['hub.verify_token']=="primayudantra"){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("wrong token")
 })
+
+app.post('/webhook/', function(req,res){
+	let messaging_events = req.body.entry[0].messaging_events;
+	for (let i=0; i < messaging_events.length; i++){
+		let event = messaging_events[i];
+		let sender = event.sender.id
+		if(event.message && event.message.text){
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0,100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text){
+	let messageData = {text: text}
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token, token},
+		method: "POST",
+		json: {
+			receipt: {id: sender},
+			message: messageData
+		}
+	}, function(error, response,body){
+		if(error){
+			console.log("sending error")
+		}else if(response.body.error){
+			console.log("response body error")
+		}
+	})
+}
 
 app.listen(app.get('port'), function(){
 	console.log("running: port")
